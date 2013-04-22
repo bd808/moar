a6cd18dcedd92157b8a9f3ce787fe7f799e61c62
Bryan Davis <bd808@bd808.com>
2013-04-21 20:02:03 -0600

    Starting on documentation.
    Will be trying to use readthedocs for documentation.

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+from datetime import date
+
+extensions = ['sphinxcontrib.phpdomain']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Moar'
+copyright = unicode(date.today().year) + u', Bryan Davis and contributors'
+version = '0.1'
+release = version
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+lexers['php'] = PhpLexer(startinline=True)
+primary_domain = 'php'
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+  html_theme = 'default'
+else:
+  html_theme = 'nature'
+
+html_static_path = ['_static']
+htmlhelp_basename = 'Moardoc'
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+================
+Moar PHP Library
+================
+
+.. code-block:: text
+
+    __  __    _______
+    \ \ \ \  | __ __ | ___  _____  ____
+     \ \ \ \ | ||_|| |/ _ \(____ |/ ___)
+     / / / / | |   | | |_| / ___ | |
+    /_/ /_/  |_|   |_|\___/\_____|_|
+
+Moar is a smallish collection of PHP libraries and frameworks maintained by
+Bryan Davis.
+
+Modules
+=======
+.. toctree::
+    :maxdepth: 1
+    :glob:
+
+    modules/**
